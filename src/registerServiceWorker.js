8d31050f416fa128a258c1ecb5fc1a61fa73d87a
nabiloo19 <nabil.alanssi19@gmail.com>
2020-01-16 00:39:14 +0300

    :white_check_mark: Service Worker Updated

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -5,28 +5,28 @@ import { register } from 'register-service-worker'
 if (process.env.NODE_ENV === 'production') {
   register(`${process.env.BASE_URL}service-worker.js`, {
     ready () {
-      console.log(
+      alert(
         'App is being served from cache by a service worker.\n' +
         'For more details, visit https://goo.gl/AFskqB'
       )
     },
     registered () {
-      console.log('Service worker has been registered.')
+      alert('Service worker has been registered.')
     },
     cached () {
-      console.log('Content has been cached for offline use.')
+      alert('Content has been cached for offline use.')
     },
     updatefound () {
-      console.log('New content is downloading.')
+      alert('New content is downloading.')
     },
     updated () {
-      console.log('New content is available; please refresh.')
+      alert('New content is available; please refresh.')
     },
     offline () {
-      console.log('No internet connection found. App is running in offline mode.')
+      alert('No internet connection found. App is running in offline mode.')
     },
     error (error) {
-      console.error('Error during service worker registration:', error)
+      alert('Error during service worker registration:', error)
     }
   })
 }
