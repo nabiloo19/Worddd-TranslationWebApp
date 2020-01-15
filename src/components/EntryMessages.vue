b82d5b980a56212708ef93a6674a1c743a532ef4
nabiloo19 <nabil.alanssi19@gmail.com>
2020-01-15 23:20:26 +0300

    ✨ Slight change made - No breakthrough

--- src/components/EntryMessages.vue
+++ src/components/EntryMessages.vue
@@ -3,7 +3,6 @@
     <v-layout text-center wrap>
       <v-flex mb-4>
         <h1 class="display-2 font-weight-bold mb-3">Welcome to <i>Worddd</i></h1>
-
         <h5>
           Built with
           <v-icon color="red" size="20px">mdi-cards-heart</v-icon>using <span style="color:#42b883">VueJS</span>
